f44dc117200c9b3d9f937d0fe1f812664fed4552
YakovlevEA <iakovlev.ea@mail.ru>
2018-11-26 16:33:46 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,20 +4,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper extends HelperBase{
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(WebDriver wd) {
         super(wd);
     }
 
-    public   void gotoGroupPage(){
-       click(By.linkText("groups"));
+    public void gotoGroupPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).equals("Group")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
+            click(By.linkText("groups"));
     }
 
     public void gotoAddNewPage() {
         click(By.linkText("add new"));
     }
+
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 
