18766759b4bcc8d5358d81782a1516e4ae2feee2
YakovlevEA <iakovlev.ea@mail.ru>
2018-12-17 15:47:22 +0300

    Xstream xml

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/generators/GroupDataGenerator.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/generators/GroupDataGenerator.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.addressbook.appmanager.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
-import ru.stqa.pft.addressbook.appmanager.model.Groups;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,6 +18,8 @@ public class GroupDataGenerator {
     public int count;
     @Parameter(names = "-f", description = "Target file")
     public String file;
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
 
     public static void main (String[] args) throws IOException {
 
@@ -36,10 +38,27 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroup(count);
-        save(groups, new File(file));
+        if (format.equals("csv")){
+            saveAsCsv(groups, new File(file));
+        } else if (format.equals("xml")){
+            saveAsXml(groups, new File(file));
+        }else{
+            System.out.println("Unrecognized format ");
+        }
+        
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xStream = new XStream();
+        //xStream.alias("group", GroupData.class);
+        xStream.processAnnotations(GroupData.class);
+        String xml = xStream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group : groups){
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
